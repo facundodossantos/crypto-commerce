30a8244b3c8bacb34ac8212bf25481613e2f95ba
Facundo <facundo.dossantos.18@gmail.com>
2022-07-13 19:16:27 -0300

    mocked data

--- src/components/ItemListContainer/Item/Item.jsx
+++ src/components/ItemListContainer/Item/Item.jsx
@@ -1,10 +1,11 @@
 const Item = ({ item, greeting }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-violet-100">
-      <img className="w-full" src={item.image} alt="Sunset in the mountains" />
+      <img className="w-full" src={item.image} alt={item.description} />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{item.title}</div>
-        <p className="text-gray-700 text-base">{item.description}</p>
+        <div className="font-bold text-xl mb-2">{item.name}</div>
+        <p className="text-gray-700 text-sm
+        ">{item.description}</p>
       </div>
       <div className="px-6 pt-4 pb-2">
         <p className="text-gray-700 text-base">{greeting}</p>
--- src/data/items.js
+++ src/data/items.js
@@ -1,9 +1,9 @@
 const items = [
     {
         id: 1,
-        name: 'NFT',
-        description: 'This is NFT 1',
-        price: '$1.00',
+        name: 'Lazy Lion #1',
+        description: 'Lazy Lions is a limited collection of unique, programmatically generated NFTs hosted on the Ethereum blockchain. Each Lazy Lion is unique - just like you ...',
+        price: '0.9 ETH',
         image: 'https://public.nftstatic.com/static/nft/zipped/510a3a7cac1747e390485f513755604e_zipped.png',
         quantity: 1
 
